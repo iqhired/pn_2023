967f7bd7397dc567c4afa296c0d427a3811a43a4
Asha M S <ashams001@gmail.com>
2023-01-05 10:23:27 +0530

    Space between fields

--- form_module/form_graph.php
+++ form_module/form_graph.php
@@ -273,7 +273,7 @@ include("../admin_menu.php");
                 <div class="card-body pt-0">
                     <div class="pd-30 pd-sm-20">
                         <div class="row row-xs align-items-center mg-b-20">
-                            <div class="col-md-2">
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Station * : </label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
@@ -348,6 +348,8 @@ include("../admin_menu.php");
                                 </select>
                             </div>
                             <div class="col-md-2">
+                            </div>
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Part Family : </label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
@@ -388,7 +390,7 @@ include("../admin_menu.php");
                             </div>
                         </div>
                         <div class="row row-xs align-items-center mg-b-20">
-                            <div class="col-md-2">
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Part Number  : </label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
@@ -417,6 +419,8 @@ include("../admin_menu.php");
                                 </select>
                             </div>
                             <div class="col-md-2">
+                            </div>
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Form Type : </label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
@@ -445,7 +449,7 @@ include("../admin_menu.php");
                             </div>
                         </div>
                         <div class="row row-xs align-items-center mg-b-20">
-                            <div class="col-md-2">
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Date From : &nbsp;</label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
@@ -457,6 +461,8 @@ include("../admin_menu.php");
                                 </div>
                             </div>
                             <div class="col-md-2">
+                            </div>
+                            <div class="col-md-1">
                                 <label class="form-label mg-b-0">Date To: &nbsp;&nbsp;</label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
