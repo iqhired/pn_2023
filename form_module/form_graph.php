c060e6c9531fa709ff7e344792505c0c0d4a1d8b
Asha M S <ashams001@gmail.com>
2023-01-05 10:27:10 +0530

    Space between fields

--- form_module/form_graph.php
+++ form_module/form_graph.php
@@ -391,7 +391,7 @@ include("../admin_menu.php");
                         </div>
                         <div class="row row-xs align-items-center mg-b-20">
                             <div class="col-md-1">
-                                <label class="form-label mg-b-0">Part Number  : </label>
+                                <label class="form-label mg-b-0">Part Number : </label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
                                 <select  name="part_number" id="part_number" class="form-control form-select select2" data-bs-placeholder="Select Country">
@@ -463,7 +463,7 @@ include("../admin_menu.php");
                             <div class="col-md-2">
                             </div>
                             <div class="col-md-1">
-                                <label class="form-label mg-b-0">Date To: &nbsp;&nbsp;</label>
+                                <label class="form-label mg-b-0">Date To : &nbsp;&nbsp;</label>
                             </div>
                             <div class="col-md-4 mg-t-5 mg-md-t-0">
                                 <div class="input-group">
