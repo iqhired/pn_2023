b71c8eada5f06ed8fdbce77a5b7d0a4a960216a3
Asha M S <ashams001@gmail.com>
2023-01-20 17:40:26 +0530

    Modified config

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 define('SITE_ROOT', __DIR__);
 
-include("config/database_config.php");
-include("config/general_coding_config.php");
+include("/Users/ashams001/Sites/SG_Projects/pn_config.php");
+//include("config/database_config.php");
+//include("config/general_coding_config.php");
 include("config/error_config.php");
 $url = "../api/";
 require_once "helper/PNUtilHelper.php";
