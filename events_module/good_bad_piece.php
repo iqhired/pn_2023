3836f907854f974d3f9c7319d76fcf795023ff31
swati <swati-chavan1>
2023-01-23 16:35:49 +0530

    new menu

--- events_module/good_bad_piece.php
+++ events_module/good_bad_piece.php
@@ -281,40 +281,15 @@ if( $actual_eff ===0 || $target_eff === 0 || $target_eff === 0.0){
         .mt-4 {
             margin-top: 0rem!important;
         }
-        .row-body {
-            display: flex;
-            flex-wrap: wrap;
-            margin-left: -8.75rem;
-            margin-right: 6.25rem;
-        }
-        @media (min-width: 320px) and (max-width: 480px) {
-            .row-body {
 
-                margin-left: 0rem;
-                margin-right: 0rem;
-            }
-        }
 
         @media (min-width: 481px) and (max-width: 768px) {
-            .row-body {
-
-                margin-left: -15rem;
-                margin-right: 0rem;
-            }
             .col-md-1 {
                 flex: 0 0 8.33333%;
                 max-width: 10.33333%!important;
             }
         }
 
-        @media (min-width: 769px) and (max-width: 1024px) {
-            .row-body {
-
-                margin-left:-15rem;
-                margin-right: 0rem;
-            }
-
-        }
 
 
         table.dataTable thead .sorting:after {
