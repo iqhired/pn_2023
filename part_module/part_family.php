0821ddc140159f2c154506c132f5fb7c9fa1a2b9
nasrathf2001 <nasrath.f@iqhired.com>
2023-02-01 12:32:30 +0530

    Fix the Functionality issue

--- part_module/part_family.php
+++ part_module/part_family.php
@@ -3,25 +3,7 @@ include("../config.php");
 //include("../sup_config.php");
 $chicagotime = date("Y-m-d H:i:s");
 $temp = "";
-
-
-//Set the session duration for 10800 seconds - 3 hours
-$duration = $auto_logout_duration;
-//Read the request time of the user
-$time = $_SERVER['REQUEST_TIME'];
-//Check the user's session exist or not
-if (isset($_SESSION['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $duration) {
-    //Unset the session variables
-    session_unset();
-    //Destroy the session
-    session_destroy();
-    header($redirect_logout_path);
-//  header('location: ../logout.php');
-    exit;
-}
-//Set the time of the user's last activity
-$_SESSION['LAST_ACTIVITY'] = $time;
-
+checkSession();
 $assign_by = $_SESSION["id"];
 
 if (count($_POST) > 0) {
