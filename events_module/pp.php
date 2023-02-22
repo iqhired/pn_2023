ed5aafd63b0a8b6a2efd8d1a93e06ed07d7e8ce8
Asha M S <ashams001@gmail.com>
2023-02-22 18:14:11 +0530

    print bug fix

--- events_module/pp.php
+++ events_module/pp.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
 <html>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <head>
-    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
-    <script src="../assets/js/BrowserPrint-3.0.216.min.js"></script>
-    <script src="../assets/js/BrowserPrint-Zebra-1.0.216.min.js"></script>
+    <script src="https://plantnavigator.com/assets/js/BrowserPrint.js"></script>
+    <script src="https://plantnavigator.com/assets/js/DevDemo.js"></script>
 </head>
 <body>
 <script>
     function ss(url) {
-        send(url);
+        sendFile(url);
     }
     function ss1(url,cnt) {
         for(var i = 1; i <= cnt; i++) {
-            send(url);
+            sendFile(url);
         }
     }
 </script>
