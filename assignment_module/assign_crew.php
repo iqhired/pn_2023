0cf0c4c82e7b6d0dcb30a42d6d37feed3edd7e39
Asha M S <ashams001@gmail.com>
2023-02-28 23:51:33 +0530

    assign crew reset bug fix

--- assignment_module/assign_crew.php
+++ assignment_module/assign_crew.php
@@ -367,6 +367,14 @@ if ($ps != "") {
     $qur04 = mysqli_query($db, "SELECT * FROM  cam_line where line_id = '$assign_line' ");
     $rowc04 = mysqli_fetch_array($qur04);
     $lnname = $rowc04["line_name"];
+    $sql_st_2022 = "update cam_station_pos_rel set assigned ='0' where line_id ='$assign_line' ";
+    mysqli_query($db, $sql_st_2022);
+    $sql_st_2021 = "SELECT * FROM cam_assign_crew WHERE line_id = '$assign_line' and resource_type = 'regular';";
+    $result_st_2021 = mysqli_query($db, $sql_st_2021);
+    while ($row_st_2021 = mysqli_fetch_array($result_st_2021)) {
+        $sql_st_2022 = "update cam_station_pos_rel set assigned ='1' where position_id	='$row_st_2021[position_id]' and line_id ='$assign_line' ";
+        mysqli_query($db, $sql_st_2022);
+    }
     $cnt = 1;
     ?>
     <form action="" method="post" id="update-form" class="form-horizontal">
