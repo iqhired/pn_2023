b151b3aa7b71f0debcce0f89d5fb163518ac9d57
nasrathf2001 <nasrath.f@iqhired.com>
2023-03-13 11:11:48 +0530

    create order-->supplier module

--- order_module/create_order.php
+++ order_module/create_order.php
@@ -1,76 +1,103 @@
-<?php
-$useragent=$_SERVER['HTTP_USER_AGENT'];
-if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4)))
-{
-    header('Location: ./config/403.php');
-}
-require "../vendor/autoload.php";
-use \Firebase\JWT\JWT;
-$status = '0';
-$message = "";
-include("../config.php");
+<?php include("../config.php");
+$import_status_message = "";
 include("../sup_config.php");
 $chicagotime = date("Y-m-d H:i:s");
 $temp = "";
+if (!isset($_SESSION['user'])) {
+    if($_SESSION['is_tab_user'] || $_SESSION['is_cell_login']){
+        header($redirect_tab_logout_path);
+    }else{
+        header($redirect_logout_path);
+    }
+}
+//Set the session duration for 10800 seconds - 3 hours
+$duration = $auto_logout_duration;
+//Read the request time of the user
+$time = $_SERVER['REQUEST_TIME'];
+//Check the user's session exist or not
+if (isset($_SESSION['LAST_ACTIVITY']) && ($time - $_SESSION['LAST_ACTIVITY']) > $duration) {
+    //Unset the session variables
+    session_unset();
+    //Destroy the session
+    session_destroy();
+    if($_SESSION['is_tab_user'] || $_SESSION['is_cell_login']){
+        header($redirect_tab_logout_path);
+    }else{
+        header($redirect_logout_path);
+    }
 
-
-     if (count($_POST) > 0) {
-     $order_name = $_POST['order_name'];
-    //create
-    if ($order_name != "") {
-    $c_id = $_POST['c_id'];
+//	header('location: ../logout.php');
+    exit;
+}
+$is_tab_login = $_SESSION['is_tab_user'];
+$is_cell_login = $_SESSION['is_cell_login'];
+//Set the time of the user's last activity
+$_SESSION['LAST_ACTIVITY'] = $time;
+/*$i = $_SESSION["role_id"];
+if ($i != "super" && $i != "admin" && $i != "pn_user" && $_SESSION['is_tab_user'] != 1 && $_SESSION['is_cell_login'] != 1 ) {
+    header('location: ../dashboard.php');
+}*/
+if (count($_POST) > 0) {
     $order_name = $_POST['order_name'];
-    $order_desc = $_POST['order_desc'];
-    $order_status_id = $_POST['order_status_id'];
-    $order_active = $_POST['order_active'];
-    $shipment_details = $_POST['shipment_details'];
-    $created_by = $_SESSION['id'];
-    $service_url = $rest_api_uri . "supplier/create_order.php";
-    $curl = curl_init($service_url);
-    $curl_post_data = array(
-        'c_id' => $c_id,
-        'order_name' => $order_name,
-        'order_desc' => $order_desc,
-        'order_status_id' => $order_status_id,
-        'order_active' => $order_active,
-        'shipment_details' => $shipment_details,
-        'created_on' => $created_on
-    );
-    $secretkey = "SupportPassHTSSgmmi";
-    $payload = array(
-        "author" => "Saargummi to HTS",
-        "exp" => time()+1000
-    );
-    try{
-        $jwt = JWT::encode($payload, $secretkey , 'HS256');
-    }catch (UnexpectedValueException $e) {
-        echo $e->getMessage();
-    }
-    $headers = array(
-        "Accept: application/json",
-        "access-token: " . $jwt . '"',
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-    $curl_response = curl_exec($curl);
-    if ($curl_response === false) {
-        $info = curl_getinfo($curl);
-        curl_close($curl);
-        die('error occured during curl exec. Additioanl info: ' . var_export($info));
-    }
-    curl_close($curl);
-    $decoded = json_decode($curl_response);
-    if (isset($decoded->status) && $decoded->status == 'ERROR') {
-        die('error occured: ' . $decoded->errormessage);
-    }
-}
-checkSession();
-$assign_by = $_SESSION["id"];
+//create
+    if ($order_name != "") {
+        $c_id = $_POST['c_id'];
+        $order_name = $_POST['order_name'];
+        $order_desc = $_POST['order_desc'];
+        $created_by = $_SESSION['id'];
 
-//old
+        $sql = "INSERT INTO `sup_order`( `c_id`, `order_name`, `order_desc`, `order_status_id`, `order_active`, `created_on`, `created_by`) VALUES ('$c_id','$order_name','$order_desc','1','1','$chicagotime','$created_by')";
+
+        $result1 = mysqli_query($sup_db, $sql);
+        if (!$result1) {
+            $_SESSION['message_stauts_class'] = 'alert-danger';
+            if($_SESSION['import_status_message'] == "")
+            {
+                $_SESSION['import_status_message'] = 'Error: Order Already Exists';
+            }
+        } else {
+            $_SESSION['message_stauts_class'] = 'alert-success';
+            $_SESSION['import_status_message'] = 'Order Created Successfully';
+        }
+    }
+
+//edit
+    $edit_order_status = $_POST['order_status'];
+    if ($edit_order_status != "") {
+        $id = $_POST['edit_id'];
+        if($edit_order_status == 6){
+            $sql111 = "update sup_order set order_active = 0,order_status_id='$_POST[order_status]' where order_id='$id'";
+            $result111 = mysqli_query($sup_db, $sql111);
+            if ($result111) {
+                $_SESSION['message_stauts_class'] = 'alert-success';
+                $_SESSION['import_status_message'] = 'Order Updated Successfully.';
+            } else {
+                $_SESSION['message_stauts_class'] = 'alert-danger';
+                if($_SESSION['import_status_message'] == "")
+                {
+                    $_SESSION['import_status_message'] = 'Error: Please Try Again.';
+                }
+            }
+
+        }else{
+//eidt logo
+            $sql = "update sup_order set order_status_id='$_POST[order_status]' where order_id='$id'";
+            $result1 = mysqli_query($sup_db, $sql);
+            if ($result1) {
+                $_SESSION['message_stauts_class'] = 'alert-success';
+                $_SESSION['import_status_message'] = 'Order Updated Successfully.';
+            } else {
+                $_SESSION['message_stauts_class'] = 'alert-danger';
+                if($_SESSION['import_status_message'] == "")
+                {
+                    $_SESSION['import_status_message'] = 'Error: Please Try Again.';
+                }
+            }
+        }
+    }
+
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
